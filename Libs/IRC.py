5f3fa91010651ddf960bcdd849a78df0e80b902b
eur0pa <europa@alessandro.pw>
2015-08-29 17:59:17 +0200

    poff

--- Libs/IRC.py
+++ Libs/IRC.py
@@ -76,7 +76,7 @@ class Client(threading.Thread):
             self.sendirc('%d giveaway nel backlog, %d filtri attivi' % (gcount, fcount))
 
         elif cmd == '!poff':
-            self.stop()
+            self._quit()
 
         elif cmd == '!filtra':
             if len(arg) < 4:
