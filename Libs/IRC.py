d55abfaff524b93d5e7afe19d59abd9681bfb32e
eur0pa <europa@alessandro.pw>
2015-08-29 20:50:19 +0200

    poff

--- Libs/IRC.py
+++ Libs/IRC.py
@@ -9,19 +9,23 @@ class Client(threading.Thread):
     """docstring for Client"""
     def __init__(self):
         threading.Thread.__init__(self)
+        self._stop = threading.Event()
+        self.s = socket.socket()
+
         self.host = Globals.Config['IRC'][0]
         self.port = int(Globals.Config['IRC'][1])
         self.nick = Globals.Config['IRC'][2]
         self.chan = Globals.Config['IRC'][3]
-        self.s = socket.socket()
 
     def run(self):
         self._login()
-        self._idle()
+        while not self._stop.isSet():
+            self._idle()
+            time.sleep(1)
+        self._quit()
 
     def stop(self):
-        self.s.close()
-        self._Thread_stop()
+        self._stop.set()
 
     def sendirc(self, msg, type=0, dest=None):
         if not dest:
@@ -40,34 +44,29 @@ class Client(threading.Thread):
         time.sleep(1)
 
     def _quit(self):
-        self.sendirc('QUIT :*poff*', type=1)
-        time.sleep(1)
-        self.stop()
+        self.sendirc('QUIT :*poff*' % i, type=1)
 
     def _idle(self):
-        while True:
-            cmd = None
-            arg = None
-            buf = self.s.recv(1024).strip().split('\n')
-            for line in buf:
-                clean = line.split()
-            try:
-                if clean[0] == 'PING':
-                    self.sendirc("PONG %s" % clean[1], type=1)
+        cmd = None
+        arg = None
+        buf = self.s.recv(1024).strip().split('\n')
+        for line in buf:
+            clean = line.split()
+        try:
+            if clean[0] == 'PING':
+                self.sendirc("PONG %s" % clean[1], type=1)
 
-                elif clean[1] == 'PRIVMSG':
-                    if clean[2] != self.chan:
-                        continue
-                    try:
-                        cmd = line.split(':')[2].split()[0]
-                        arg = line.split(':')[2][len(cmd)+1:]
-                        self._handle_commands(cmd, arg)
-                    except:
-                        pass
-            except Exception, err:
-                print '>>> IRC EXCEPTION <<< [ ' + str(err) + ' ]'
-                pass
-            time.sleep(1)
+            elif clean[1] == 'PRIVMSG':
+                if clean[2] != self.chan:
+                    return
+                try:
+                    cmd = line.split(':')[2].split()[0]
+                    arg = line.split(':')[2][len(cmd)+1:]
+                    self._handle_commands(cmd, arg)
+                except:
+                    pass
+        except Exception, err:
+            pass
 
     def _handle_commands(self, cmd, arg):
         if cmd == '!stato':
@@ -76,7 +75,7 @@ class Client(threading.Thread):
             self.sendirc('%d giveaway nel backlog, %d filtri attivi' % (gcount, fcount))
 
         elif cmd == '!poff':
-            self._quit()
+            self.stop()
 
         elif cmd == '!filtra':
             if len(arg) < 4:
