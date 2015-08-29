0c38d7306d29985f1b98d23c6827d669f9d5a732
eur0pa <europa@alessandro.pw>
2015-08-29 12:47:15 +0200

    moved config and backlog reading to Init() class

--- bot.py
+++ bot.py
@@ -2,42 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import threading
-
-from time import sleep
+import time
 
 from Libs import IRC
 from Libs import Lurker
 from Libs import Globals
-
-
-def read_backlog(backlog_file):
-    backlog = {}
-    try:
-        with open(backlog_file) as f:
-            for entry in f.read().split(','):
-                backlog[entry] = True
-    except IOError:
-        file(backlog_file, 'w').close()
-    return backlog
-
-
-def read_config(cfg_file):
-    config = {}
-    with open(cfg_file) as f:
-        for line in f.readlines():
-            line = line.strip()
-            if not line:
-                continue
-            if line[0:2] == '>>':
-                values = []
-                section = line.split('>>')[1]
-            else:
-                if len(line.split('=')) > 1:
-                    values.append(line.split('=')[1])
-                else:
-                    values.append(line)
-            config[section] = values
-    return config
 
 
 def main():
@@ -45,11 +14,9 @@ def main():
     lurk_thread = None
     threads = []
 
+    Globals.Init()
     utils = Globals.Utils()
     utils.Print("reddit giveaway grabber\n")
-
-    Globals.Config = read_config(Globals.CONFIG_FILE)
-    Globals.Backlog = read_backlog(Globals.BACKLOG_FILE)
 
     while threading.active_count() > 0:
         if not irc_thread or not irc_thread.is_alive():
@@ -58,7 +25,7 @@ def main():
             irc_thread.daemon = True
             threads.append(irc_thread)
             irc_thread.start()
-            sleep(1)
+            time.sleep(1)
         irc_thread.join(1)
 
 if __name__ == '__main__':
