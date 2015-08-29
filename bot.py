b4cf06dd036403a2b55b912688c9b74b4136fd95
eur0pa <europa@alessandro.pw>
2015-08-29 15:09:31 +0200

    pprint -> sprint

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import threading
-import time
+
 import sys
+import time
+import threading
 
 from Libs import IRC
 from Libs import Lurker
@@ -21,11 +22,11 @@ def main():
 
     Globals.Init()
     u = Globals.Utils()
-    u.pprint("reddit giveaway grabber\n")
+    u.sprint("reddit giveaway grabber\n")
 
     while threading.active_count() > 0:
         if not irc_thread or not irc_thread.is_alive():
-            u.pprint('starting irc thread...\n')
+            u.sprint('starting irc thread...\n')
             irc_thread = IRC.Client()
             irc_thread.daemon = True
             threads.append(irc_thread)
@@ -34,13 +35,14 @@ def main():
         irc_thread.join(1)
 
         if not lurk_thread or not lurk_thread.is_alive():
-            u.pprint('starting reddit thread...\n')
+            u.sprint('starting reddit thread...\n')
             lurk_thread = Lurker.Lurk(irc_thread)
             lurk_thread.daemon = True
             threads.append(lurk_thread)
             lurk_thread.start()
             time.sleep(1)
         lurk_thread.join(1)
+
 
 if __name__ == '__main__':
     main()
